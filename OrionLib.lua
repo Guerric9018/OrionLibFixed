0726f41d9e189244a3d5434c6aee8ff803553706
Guerric9018 <88341104+Guerric9018@users.noreply.github.com>
2024-05-13 12:49:03 +0200

    Update OrionLib.lua

--- OrionLib.lua
+++ OrionLib.lua
@@ -1224,7 +1224,7 @@ function OrionLib:MakeWindow(WindowConfig)
 				end  
 
 				function Dropdown:Title(Text)
-					DropdownFrame.F.Content.Text = Text
+					DropdownFrame.Content.Text = Text
 				end
 
 				function Dropdown:Set(Value)
